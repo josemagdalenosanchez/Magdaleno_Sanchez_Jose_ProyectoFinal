73749b32a670772ed2ccad9e864458b2743dc19c
Jose Magdaleno <josemagdalenosanchez@gmail.com>
2018-01-11 09:29:05 +0100

    Primer commit

---
+++ admin/Usuarios-ADMIN.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion de Reparaciones</title>
+  </head>
+  <body>
+    <?php
+
+      //CREATING THE CONNECTION
+      $connection = new mysqli("localhost", "root", "2asirtriana", "MIDGARD");
+      $connection->set_charset("utf8");
+
+      //TESTING IF THE CONNECTION WAS RIGHT
+      if ($connection->connect_errno) {
+          printf("Connection failed: %s\n", $connection->connect_error);
+          exit();
+      }
+
+      //MAKING A SELECT QUERY
+      /* Consultas de selección que devuelven un conjunto de resultados */
+      if ($result = $connection->query("select * from Miembros;")) {
+
+         
+
+      ?>
+
+          <!-- PRINT THE TABLE AND THE HEADER -->
+          <table style="border:1px solid black">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellidos</th>
+              <th>Fecha de Nacimiento</th>
+              <th>Fecha de Ingreso</th>
+              <th>Tipo</th>
+              <th>E-Mail</th>
+              <th>ACCIONES</th>    
+          </thead>
+
+      <?php
+
+          //FETCHING OBJECTS FROM THE RESULT SET
+          //THE LOOP CONTINUES WHILE WE HAVE ANY OBJECT (Query Row) LEFT
+          while($obj = $result->fetch_object()) {
+              //PRINTING EACH ROW
+              echo "<tr>";
+              echo "<td>".$obj->Nombre."</td>";
+              echo "<td>".$obj->Apellidos."</td>";
+              echo "<td>".$obj->FechaNacimiento."</td>";
+              echo "<td>".$obj->FechaIngreso."</td>";
+              echo "<td>".$obj->Tipo."</td>";
+              echo "<td>".$obj->EMail."</td>";
+              echo "<td>".
+                   "</td>";
+              echo "</tr>";
+          }
+
+          //Free the result. Avoid High Memory Usages
+          $result->close();
+          unset($obj);
+          unset($connection);
+
+      } //END OF THE IF CHECKING IF THE QUERY WAS RIGHT
+
+    ?>
+  </body>
+</html>
---
+++ user/Inicio.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="./Estilos/1.css
+                                 ">
+</head>
+
+<body>
+    <div id="general">
+        <div id="banner">
+            <div id="imagen">
+            <div id="bregistro"></div>
+            </div>
+            <div id="datos">
+                <div id="user"></div>
+                <div id="contraseña"></div>
+                <div id="submit"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
